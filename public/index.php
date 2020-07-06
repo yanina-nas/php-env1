58fd62487ea72a7f87e8c191b5187088b23ae51a
Anastasiia Ianina <bait@MacBook-Air-2.local>
2020-07-06 13:31:41 +0300

    First commit

--- public/index.php
+++ public/index.php
@@ -1,3 +1,6 @@
-<?php
-
-echo 'INDEX';
+<?php require_once '../vendor/autoload.php';?>
+<form action="test.php" method="post">
+ <p>Name: <input type="text" name="name" /></p>
+ <p>Phone number: <input type="text" name="phone" /></p>
+ <p><input type="submit" /></p>
+</form>
--- public/test.php
+++ public/test.php
@@ -1,3 +1,5 @@
-<?php
-
-echo 'TEST';
+<?php require_once '../vendor/autoload.php'; ?>
+<?php echo 'Hello, '; ?>
+<?php echo htmlspecialchars($_POST['name']); ?>
+<?php echo nl2br("\n Your age is ");?>
+<?php echo (int)$_POST['phone']; ?>
