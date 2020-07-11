f8599a5fa266728fe3f9a3c38bb5c66322210a1c
yanina-nas <46354296+yanina-nas@users.noreply.github.com>
2020-07-11 08:22:23 +0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -4,9 +4,4 @@
  <p>Phone number: <input type="text" name="phone" /></p>
  <p><input type="submit" /></p>
 </form>
-<?php $app = new \Slim\App();
-$app->get('/test1', function ($request, $response, $args) {echo '1';});
-$app->get('/test2', function ($request, $response, $args) {echo '2';});
-$app->any('/books/[{id}]', function ($request, $response, $args) {
-});
-$app->run();
+
